93e3008818ddba6d7ee35b123dc67372e9ca5aa0
Niya Panamdanam <niya.panamdanam@rigor.com>
2019-10-17 10:32:22 -0400

    Start of memoization

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'flexboxgrid'
 import './App.css'
 import UserList from './UserList'
 
-class App extends React.Component {
-  state = {
-    data: null
-  }
+function App () {
+  const [data, setData] = useState(null)
 
-  loadData = () => {
+  const loadData = () => {
     fetch('/data.json')
       .then(response => response.json())
-      .then(data => this.setState({ data }))
+      .then(data => setData(data))
   }
 
-  render () {
-    const { data } = this.state
-    return (
-      <div className="App row">
-        <header className='col-xs-12'>
-          Perfomance Test
-        </header>
-        <div className='col-xs-12 row content'>
-          {
-            data == null ?
-              <button onClick={this.loadData}>Load User Data</button>
-              : <UserList data={data} />
-          }
-        </div>
+  return (
+    <div className="App row">
+      <header className='col-xs-12'>
+        Perfomance Test
+      </header>
+      <div className='col-xs-12 row content'>
+        {
+          data == null ?
+            <button onClick={loadData}>Load User Data</button>
+            : <UserList data={data} />
+        }
       </div>
-    )
-  }
-
+    </div>
+  )
 }
 
 export default App
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -8,20 +8,17 @@ const UserList = ({ data }) => {
 
 export default UserList
 
-class ListItem extends React.Component {
-  render () {
-    const { users } = this.props
-    return (
-      users.map(user => (
-        <div className='col-xs-3' key={user.id} style={{ marginBottom: '40px' }}>
-          <div className='boxStyles'>
-            <h4>{user.name}</h4>
-            <p>Company: {user.company}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone: {user.phone}</p>
-          </div>
+const ListItem = ({ users }) => {
+  return (
+    users.map(user => (
+      <div className='col-xs-3' key={user.id} style={{ marginBottom: '40px' }}>
+        <div className='boxStyles'>
+          <h4>{user.name}</h4>
+          <p>Company: {user.company}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
         </div>
-      ))
-    )
-  }
+      </div>
+    ))
+  )
 }
